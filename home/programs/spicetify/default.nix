c7239516942dd88eb1b0ac2e059a7b5866c38dd1
aqu4holic <gautruc.anh1234@gmail.com>
2024-09-08 21:03:34 +0700

    update(spicetify): remove this temporarily

--- home/programs/spicetify/default.nix
+++ home/programs/spicetify/default.nix
@@ -13,25 +13,25 @@ in
         spotify
     ];
 
-    programs.spicetify = {
-        enable = false;
-        # theme = spicePkgs.themes.catppuccin;
-        # colorScheme = "mocha";
-
-        enabledExtensions = with spicePkgs.extensions; [
-            fullAppDisplayMod
-            shuffle # shuffle+ (special characters are sanitized out of ext names)
-            hidePodcasts
-            genre
-            powerBar
-            playlistIcons
-            goToSong
-            songStats
-            showQueueDuration
-            copyToClipboard
-            history
-            playNext
-            volumePercentage
-        ];
-    };
+    # programs.spicetify = {
+    #     enable = false;
+    #     # theme = spicePkgs.themes.catppuccin;
+    #     # colorScheme = "mocha";
+    #
+    #     enabledExtensions = with spicePkgs.extensions; [
+    #         fullAppDisplayMod
+    #         shuffle # shuffle+ (special characters are sanitized out of ext names)
+    #         hidePodcasts
+    #         genre
+    #         powerBar
+    #         playlistIcons
+    #         goToSong
+    #         songStats
+    #         showQueueDuration
+    #         copyToClipboard
+    #         history
+    #         playNext
+    #         volumePercentage
+    #     ];
+    # };
 }
