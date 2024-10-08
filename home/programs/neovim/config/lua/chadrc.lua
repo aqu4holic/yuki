ed99d741ebd0c0e9b2e1970d3a9d972c4f9a411d
aqu4holic <gautruc.anh1234@gmail.com>
2024-10-08 11:28:21 +0700

    update(nvim): thanks to @HynDuf

--- home/programs/neovim/config/lua/chadrc.lua
+++ home/programs/neovim/config/lua/chadrc.lua
@@ -1,13 +1,6 @@
 local M = {}
 
 M.ui = {
-    hl_add = require("highlights").add,
-	hl_override = require("highlights").override,
-
-    -- theme = "catppuccin",
-    theme = "tokyodark",
-    transparency = true,
-
     tabufline = {
         order = { "treeOffset", "buffers", "tabs" },
     },
@@ -15,6 +8,13 @@ M.ui = {
     statusline = {
         theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
     },
+}
+
+M.base46 = {
+	hl_add = require("highlights").add,
+	hl_override = require("highlights").override,
+	theme = "tokyodark",
+	transparency = true,
 }
 
 return M
