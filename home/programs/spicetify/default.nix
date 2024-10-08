66ad81a3feeb3af60c25c4613a390b6d53de45fb
aqu4holic <gautruc.anh1234@gmail.com>
2024-10-08 11:28:47 +0700

    update(spicetify): as the name

--- home/programs/spicetify/default.nix
+++ home/programs/spicetify/default.nix
@@ -1,37 +1,37 @@
 { config, pkgs, lib, inputs, ... }:
 let
-    spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in
 {
     nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
         "spotify"
     ];
 
-    imports = [ inputs.spicetify-nix.homeManagerModule ];
+    # imports = [ inputs.spicetify-nix.homeManagerModule ];
 
-    home.packages = with pkgs; [
-        spotify
-    ];
+    # home.packages = with pkgs; [
+    #     spotify
+    # ];
 
-    # programs.spicetify = {
-    #     enable = false;
-    #     # theme = spicePkgs.themes.catppuccin;
-    #     # colorScheme = "mocha";
-    #
-    #     enabledExtensions = with spicePkgs.extensions; [
-    #         fullAppDisplayMod
-    #         shuffle # shuffle+ (special characters are sanitized out of ext names)
-    #         hidePodcasts
-    #         genre
-    #         powerBar
-    #         playlistIcons
-    #         goToSong
-    #         songStats
-    #         showQueueDuration
-    #         copyToClipboard
-    #         history
-    #         playNext
-    #         volumePercentage
-    #     ];
-    # };
+    programs.spicetify = {
+        enable = true;
+        theme = spicePkgs.themes.catppuccin;
+        colorScheme = "mocha";
+
+        enabledExtensions = with spicePkgs.extensions; [
+            fullAppDisplayMod
+            shuffle # shuffle+ (special characters are sanitized out of ext names)
+            hidePodcasts
+            # genre
+            powerBar
+            playlistIcons
+            goToSong
+            songStats
+            showQueueDuration
+            copyToClipboard
+            history
+            playNext
+            volumePercentage
+        ];
+    };
 }
