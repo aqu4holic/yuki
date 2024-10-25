dc16ae24af42f172786c9f52dfa3f57226041761
aqu4holic <gautruc.anh1234@gmail.com>
2024-10-25 16:00:06 +0700

    update(visual): change some keybinds and misc

--- home/visuals/picom/default.nix
+++ home/visuals/picom/default.nix
@@ -2,13 +2,14 @@
 {
     services.picom = {
         enable = true;
-
-        # package = pkgs.fetchFromGitHub {
-        #     owner = "FT-Labs";
-        #     repo = "picom";
-        #     rev = "df4c6a3d9b11e14ed7f3142540babea4c775ddb1";
-        #     hash = "sha256-FmORxY7SLFnAmtQyC82sK36RoUBa94rJ7BsDXjXUCXk=";
-        # };
+        # package = pkgs.picom.overrideAttrs(o: {
+        #     src = pkgs.fetchFromGitHub {
+        #         owner = "FT-Labs";
+        #         repo = "picom";
+        #         rev = "df4c6a3d9b11e14ed7f3142540babea4c775ddb1";
+        #         hash = "sha256-FmORxY7SLFnAmtQyC82sK36RoUBa94rJ7BsDXjXUCXk=";
+        #     };
+        # });
     };
 
     xdg.configFile."picom/picom.conf" = {
